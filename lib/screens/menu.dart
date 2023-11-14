ebccae0c1f432ff30aad908e6278b77aeb11bc92
Arxeno <rmfbkw04@gmail.com>
2023-11-14 16:04:40 +0700

    move inventory card file

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inventory_merch_flutter/widgets/inventory_card.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
@@ -62,59 +63,6 @@ class MyHomePage extends StatelessWidget {
                 }).toList(),
               ),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class InventoryItem {
-  final String name;
-  final IconData icon;
-  final Color backgroundColor;
-
-  InventoryItem(this.name, this.icon, this.backgroundColor);
-}
-
-class InventoryCard extends StatelessWidget {
-  final InventoryItem item;
-
-  const InventoryCard(this.item, {super.key}); // Constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: item.backgroundColor,
-      child: InkWell(
-        // Area responsive terhadap sentuhan
-        onTap: () {
-          // Memunculkan SnackBar ketika diklik
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-        },
-        child: Container(
-          // Container untuk menyimpan Icon dan Text
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.black54,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.black54),
-                ),
-              ],
-            ),
           ),
         ),
       ),
